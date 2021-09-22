4ef24a16faab91f98ad2867b9b28cdff2ee0578c
ali <ali.farnudi@ens-lyon.fr>
2021-09-22 17:17:53 +0200

    First Object Oriented version.

--- 2021/Day2_OO/orbit_OO.py
+++ 2021/Day2_OO/orbit_OO.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
+
+class Planet():
+    def __init__(self):
+        self.xy  = np.random.rand(2)*2-1
+        self.vxy = self.genVelocities()  
+    def genVelocities(self):
+        vxy = np.zeros(2)
+        vxy[0]=np.sqrt(1/(1+self.xy[0]**2/self.xy[1]**2))
+        vxy[1]=-vxy[0]*self.xy[0]/self.xy[1]
+        return vxy
+    def getCoords(self):
+        return self.xy
+    def evolveTimeStep(self):
+        self.xy+=self.vxy
+
 fig, ax = plt.subplots()
 ax.set_xlim(-1,1)
 ax.set_ylim(-1,1)
@@ -13,31 +28,21 @@ ax.set_aspect('equal')
 
 ax.plot([0],[0],'o',ms=30, c='gold')
 
-planetCoords = np.asarray([0.7,0])
-planetVels   = np.asarray([0.0,0.5])*0.01
+earth = Planet()
 
 G = 0.00002
 line1, =ax.plot([],[],'b.',ms=20)
 
-def animate(i,planetCoords,planetVels):
-    deltaRMag = np.sqrt(np.sum(np.square(planetCoords)))
-    AcceDir  = -planetCoords/deltaRMag
-    AcceMag  = G/deltaRMag**2
-    Acce_n_1  = AcceMag*AcceDir
+print(earth.getCoords())
+
+def animate(i,earth):
+    earth.evolveTimeStep()    
     
-    planetCoords += 0.5*AcceMag*AcceDir + planetVels
-    deltaRMag = np.sqrt(np.sum(np.square(planetCoords)))
-    AcceDir  = -planetCoords/deltaRMag
-    AcceMag  = G/deltaRMag**2
-    
-    planetVels   += 0.5*(AcceMag*AcceDir + Acce_n_1) 
-    
-    
-    line1.set_data(planetCoords[0],planetCoords[1])  # update the data
+    line1.set_data(earth.getCoords()[0],earth.getCoords()[1])  # update the data
     return line1,
 
 ani = animation.FuncAnimation(fig, animate, 
-                              fargs=(planetCoords,planetVels),
+                              fargs=(earth,),
                               interval=2, 
                               blit=True,
                               )
