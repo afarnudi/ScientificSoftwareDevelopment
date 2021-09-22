d44afd571fd89f624376966c5b43e6ca385f35f4
ali <ali.farnudi@ens-lyon.fr>
2021-09-22 13:54:50 +0200

    setup plot and Sun

--- 2021/Day2_OO/orbit_structural.py
+++ 2021/Day2_OO/orbit_structural.py
@@ -1,3 +1,12 @@
 #Setup a plot with the sun and a planet
 # I will create an animation scene
 # I will make the planet move.
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.set_xlim(-1,1)
+ax.set_ylim(-1,1)
+ax.set_aspect('equal')
+
+ax.plot([0],[0],'o',ms=30, c='gold')
