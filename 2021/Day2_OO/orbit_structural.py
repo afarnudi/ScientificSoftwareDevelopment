c6071165669d6147fce2c735cfc19cc577aeeb07
ali <ali.farnudi@ens-lyon.fr>
2021-09-22 14:05:41 +0200

    Added animation + Euler gravity implamentation.

--- 2021/Day2_OO/orbit_structural.py
+++ 2021/Day2_OO/orbit_structural.py
@@ -3,6 +3,8 @@
 # I will make the planet move.
 
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 ax.set_xlim(-1,1)
@@ -10,3 +12,33 @@ ax.set_ylim(-1,1)
 ax.set_aspect('equal')
 
 ax.plot([0],[0],'o',ms=30, c='gold')
+
+planetCoords = np.asarray([0.7,0])
+planetVels   = np.asarray([0.0,0.5])*0.01
+
+G = 0.00002
+line1, =ax.plot([],[],'b.',ms=20)
+
+def animate(i,planetCoords,planetVels):
+    deltaRMag = np.sqrt(np.sum(np.square(planetCoords)))
+    AcceDir  = -planetCoords/deltaRMag
+    AcceMag  = G/deltaRMag**2
+    
+    planetCoords += 0.5*AcceMag*AcceDir + planetVels
+    planetVels   += AcceMag*AcceDir 
+    
+    
+    line1.set_data(planetCoords[0],planetCoords[1])  # update the data
+    return line1,
+
+ani = animation.FuncAnimation(fig, animate, 
+                              fargs=(planetCoords,planetVels),
+                              interval=2, 
+                              blit=True,
+                              )
+
+
+
+
+
+
