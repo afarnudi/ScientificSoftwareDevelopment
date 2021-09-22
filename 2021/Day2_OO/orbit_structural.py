7c9d57d6bfa17221d8b425e89a274da13f204b0e
ali <ali.farnudi@ens-lyon.fr>
2021-09-22 15:13:13 +0200

    Factorised code into models and added a tail.

--- 2021/Day2_OO/orbit_structural.py
+++ 2021/Day2_OO/orbit_structural.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-from orbit_structural_modules import planetCoordInit
+from orbit_structural_modules import planetInit, VelocityVerlet
 
 fig, ax = plt.subplots()
 ax.set_xlim(-1,1)
@@ -15,34 +15,27 @@ ax.set_aspect('equal')
 
 ax.plot([0],[0],'o',ms=30, c='gold')
 
+planetCoords, planetVels = planetInit()
 
-planetCoords = planetCoordInit()
-planetVels   = np.asarray([0.0,0.5])*0.01
-
+tailLength=200
+tail = np.ones((tailLength,2))*planetCoords
 
 line1, =ax.plot([],[],'b.',ms=20)
+line2, =ax.plot([],[],'b',lw=2)
 
-def animate(i,planetCoords,planetVels):
-    #VelocityVerlet
+def animate(i,planetCoords,planetVels,tail, line, tailLine):
     G = 0.00002
-    deltaRMag = np.sqrt(np.sum(np.square(planetCoords)))
-    AcceDir  = -planetCoords/deltaRMag
-    AcceMag  = G/deltaRMag**2
-    Acce_n_1  = AcceMag*AcceDir
+    planetCoords, planetVels = VelocityVerlet(planetCoords, planetVels, G)
     
-    planetCoords += 0.5*AcceMag*AcceDir + planetVels
-    deltaRMag = np.sqrt(np.sum(np.square(planetCoords)))
-    AcceDir  = -planetCoords/deltaRMag
-    AcceMag  = G/deltaRMag**2
+    tail[:-1] = tail[1:]
+    tail[-1]  = planetCoords
     
-    planetVels   += 0.5*(AcceMag*AcceDir + Acce_n_1) 
-    
-    
-    line1.set_data(planetCoords[0],planetCoords[1])  # update the data
-    return line1,
+    line.set_data(planetCoords[0],planetCoords[1])  # update the data
+    tailLine.set_data(tail[:,0],tail[:,1])
+    return line, tailLine
 
 ani = animation.FuncAnimation(fig, animate, 
-                              fargs=(planetCoords,planetVels),
+                              fargs=(planetCoords,planetVels,tail, line1, line2),
                               interval=2, 
                               blit=True,
                               )
